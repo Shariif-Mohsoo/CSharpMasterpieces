a66ce3eb59abd614c3f20b1ef6fcae5541a2b405
Shariif-Mohsoo <mr8781256@gmail.com>
2025-04-25 16:42:25 +0500

    Using Class Library

---
+++ ConsoleApps/1_ConsoleApp1/1_ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using PublicClassLibrary1;
+namespace _1_ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+            Console.WriteLine(obj.sayHi());
+            Console.ReadKey();
+        }
+    }
+}
