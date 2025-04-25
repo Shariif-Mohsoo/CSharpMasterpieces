1d935f7396a57295e0ef5534734332ae3f1b5afc
Shariif-Mohsoo <mr8781256@gmail.com>
2025-04-25 16:45:16 +0500

    Public Class Library

---
+++ ClassLibraries/PublicClassLibrary1/PublicClassLibrary1/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace PublicClassLibrary1
+{
+    public class Class1
+    {
+        public string sayHi()
+        {
+            return "Hey What's up bro";
+        }
+    }
+}
---
+++ ClassLibraries/PublicClassLibrary1/PublicClassLibrary1/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PublicClassLibrary1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PublicClassLibrary1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6c3e9ec8-a957-41b7-85d9-a3ba59549cbc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyKeyFile("E:\\C#\\PublicClassLibrary1\\PublicClassLibrary.snk")]
