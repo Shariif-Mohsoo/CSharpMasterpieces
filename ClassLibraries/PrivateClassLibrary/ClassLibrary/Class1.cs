a4ca3cf7325dbd38373dd0dfdb7045614b809802
Shariif-Mohsoo <mr8781256@gmail.com>
2025-04-27 18:31:20 +0500

    Private class library

---
+++ ClassLibraries/PrivateClassLibrary/ClassLibrary/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    //internal class Class1 //it will not be accessible outside the assembly/project.
+    //private class Class1 //it should not be private otherwise build failed.
+    //protected class Class1 //it should not be protected otherwise build failed.
+    //protected internal class Class1 //it should not be protected internal otherwise build failed.
+    public class Class1
+    {
+        public string print()
+        {
+            return "Hi You Press me!";
+        }
+    }
+}
