842142fa13247b98db57ac35b6577b90a9895aef
Shariif-Mohsoo <mr8781256@gmail.com>
2025-05-03 16:55:38 +0500

    Multicase delegates with return functions

---
+++ ConsoleApps/DelegatesConsoleApp1/DelegatesConsoleApp1/MulticastDelegateReturn.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesConsoleApp1
+{
+    public delegate int GetNumber();
+    class MulticastDelegateReturn
+    {
+        public static int First()
+        {
+            return 1;
+        }
+
+        public static int Second()
+        {
+            return 2;
+        }
+
+        //public static void Main()
+        //{
+        //    GetNumber num = First;
+        //    num += Second;
+
+        //    int result = num();  // Only returns from Second
+        //    Console.WriteLine(result);  // Output: 2
+        //}
+
+    }
+}
