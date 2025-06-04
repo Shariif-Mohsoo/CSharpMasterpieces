5c3aee8920cff4aef9bfa8c9c7dada3939059084
Shariif-Mohsoo <mr8781256@gmail.com>
2025-06-04 23:34:12 +0500

    First windows form app (basic)

---
+++ WindowsForms/1_WindowsFormsApp1/1_WindowsFormsApp1/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+using ClassLibrary;
+
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows.Forms;
+
+namespace _1_WindowsFormsApp1
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1 obj1 = new Class1();
+            MessageBox.Show(obj1.print());
+        }
+    }
+}
