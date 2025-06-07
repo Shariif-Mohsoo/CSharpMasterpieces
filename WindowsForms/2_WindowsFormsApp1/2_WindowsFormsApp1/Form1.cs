0c8751246dbdded1aa25670ffdfbdd5cc843306d
Shariif-Mohsoo <mr8781256@gmail.com>
2025-06-07 18:15:10 +0500

    Windows form app basic

---
+++ WindowsForms/2_WindowsFormsApp1/2_WindowsFormsApp1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PublicClassLibrary1;
+
+namespace _2_WindowsFormsApp1
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1 obj = new Class1();
+            MessageBox.Show(obj.sayHi());
+        }
+    }
+}
