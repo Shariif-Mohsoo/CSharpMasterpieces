3fea6e6bf222c0b83fc98bddeccd83f702931f90
Shariif-Mohsoo <mr8781256@gmail.com>
2025-06-17 19:17:54 +0500

    Basic form

--- WindowsForms/LinkingTwoForms_WindowsFormApp/Form1.cs
+++ WindowsForms/LinkingTwoForms_WindowsFormApp/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 
 namespace TestWindowsFormsApp2
 {
+    
     public partial class Form1 : Form
     {
         public Form1()
@@ -24,15 +25,15 @@ namespace TestWindowsFormsApp2
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string nme = name.Text;
-            string pass = password.Text;
-            this.Close();
+           // string nme = name.Text;
+            //string pass = password.Text;
+            //this.Close();
             //Form2 form = new Form2(nme,pass);
             //check this mohsoo
-            Form2 form = new Form2();
-            form.textBox1.Text = nme;
-            form.textBox2.Text = pass;
-            form.Show();
+            //Form2 form = new Form2();
+            //form.name.Text = name.Text;
+            //form.textBox2.Text = pass;
+            //form.Show();
             //MessageBox.Show("Name: " + nme);
             //MessageBox.Show("Password: " + pass);
             
